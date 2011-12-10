af86d2dfcd54b9f2b5e622bd087f5b02110cc5af
Ian Wehrman <iwehrman@gmail.com>
2011-12-10 15:58:29 -0600

    Fix tab indices

--- run/forms.py
+++ run/forms.py
@@ -3,27 +3,26 @@ from run.models import UserProfile
 from django.contrib.auth.models import User
 
 class UserProfileForm(ModelForm):
-    birthday = DateField(widget=DateInput(format='%m/%d/%Y',attrs={'class': 'span3'}), 
+    birthday = DateField(widget=DateInput(format='%m/%d/%Y',
+        attrs={'class': 'span3', 'tabindex':'8'}), 
         label='Birthdate')
-    
-    
+        
     class Meta:
         model = UserProfile
         exclude = ('last_shoe', 'user',)
         widgets = {
-            'gender': Select(attrs={'class': 'span2'}, 
+            'gender': Select(attrs={'class': 'span2', 'tabindex': '6'}, 
                 choices=((None, 'n/a'), (True, 'Male'), (False, 'Female'),)),
-            'weight': TextInput(attrs={'class': 'span2'}),
-            # 'birthday': DateInput(attrs={'class': 'span3', }),
-            'resting_heart_rate': TextInput(attrs={'class': 'span2'}),
+            'weight': TextInput(attrs={'class': 'span2', 'tabindex': '7'}),
+            'resting_heart_rate': TextInput(attrs={'class': 'span2', 'tabindex': '9'}),
         }
         
 class UserForm(ModelForm): 
     
     new_password1 = CharField(required=False,label='New password?',
-        widget=PasswordInput())
+        widget=PasswordInput(attrs={'tabindex':'4'}))
     new_password2 = CharField(required=False,label='(repeat)',
-        widget=PasswordInput())
+        widget=PasswordInput(attrs={'tabindex':'5'}))
         
     def clean_new_password2(self):
         np1 = self.cleaned_data['new_password1']
@@ -37,7 +36,10 @@ class UserForm(ModelForm):
     
     class Meta: 
         model = User
-        widgets = {'first_name': TextInput(attrs={'tabindex':'1'})}
+        widgets = {'first_name': TextInput(attrs={'tabindex':'1'}),
+            'last_name': TextInput(attrs={'tabindex':'2'}),
+            'email': TextInput(attrs={'tabindex':'3'}),
+            }
         exclude = ['username', 'password', 'is_staff', 'is_active', 
             'is_superuser', 'last_login', 'date_joined', 'groups', 
             'user_permissions']

265ae0bea8a411af7b361fd6229aa1725b3da87d
Ian Wehrman <iwehrman@gmail.com>
2012-01-07 13:11:59 -0600

    Fix the set_calories management command

--- run/management/commands/set_calories.py
+++ run/management/commands/set_calories.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from fit.run.models import Run
+from run.models import Run
 
 class Command(BaseCommand):
     # args = '<username file.csv>'

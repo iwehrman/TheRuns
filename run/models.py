84eadb03eedcf4396e8b1b882442651a2a684fb4
Ian Wehrman <iwehrman@gmail.com>
2012-09-02 12:32:19 -0500

    don't set a default birthdate on new profiles

--- run/models.py
+++ run/models.py
@@ -307,7 +307,7 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     
     if created: 
-        UserProfile.objects.create(user=instance, birthday=date.today())
+        UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
 

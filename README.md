dcf3fcb7df72ceecf6478f91b4b37c9edc42f53c
Ian Wehrman <iwehrman@gmail.com>
2012-09-02 12:01:56 -0400

    updates reflecting new usage of virtualenv

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Python package dependencies: 
 
+0. django
 1. python-memcached
 2. gunicorn
+3. django-recaptcha-field
--- restart_gunicorn.sh
+++ restart_gunicorn.sh
@@ -2,4 +2,6 @@
 
 . /home/iwehrman/fit_env/bin/activate
 
+kill `cat gunicorn.pid` 
+sleep 3s
 python manage.py run_gunicorn 127.0.0.1:1337 --access-logfile=/home/iwehrman/fit/access.log --error-logfile=/home/iwehrman/fit/error.log --pid=/home/iwehrman/fit/gunicorn.pid -D
